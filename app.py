890188fd1eb19afebd4a1a57bcbddad87653cefc
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-12 11:14:58 -0800

    fixed the empty spaces

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if os.path.isfile('save.txt'):
     with open('save.txt', 'r') as f:
         tempApps = f.read()
         tempApps =  tempApps.split(',')
-        apps = tempApps
+        apps = [x for x in tempApps if x.strip()]
 
 
 
